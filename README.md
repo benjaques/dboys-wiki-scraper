467e202e5d2cc47c29bfa1533f753412835b2064
benjaques <apps.benj@gmail.com>
2019-08-24 20:01:21 -0400

    now pulls all regular episode forks and prints to std out

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Doughoys Rating Scraper
+# Doughboys Rating Scraper
 
 Simple Python script to scrape raw html from the doughboys wikia and output the data into a CSV.
--- episode.py
+++ episode.py
@@ -4,9 +4,14 @@
 class Episode:
     """ Wikia Episode Object """
 
-    def __init__(self, title, number):
+    def __init__(self, title, number, href, release_date, fork_ratings):
         self.title = title
         self.number = number
-        self.release_date = None
-        self.href = None
+        self.href = href
+        self.release_date = release_date
+        self.fork_ratings = fork_ratings
         self.notes = None
+
+    def __repr__(self):
+        return self.title + "\nEpisode " + self.number + "\nReleased " + self.release_date + "\nFork Ratings: " + \
+               str(self.fork_ratings)
